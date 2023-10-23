80a8ba94a07d1d46ef0dd95fb6d96042d48cf624
Lintangeaa <lindprak@gmail.com>
2023-10-23 19:38:59 +0700

    ada yang ketinggalan

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+aapp.use(cors({ origin: '*' }));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
