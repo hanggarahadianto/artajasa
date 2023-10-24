c2f946d2f5c34140418bfd9b1b319ead6f1220d4
Lintangeaa <lindprak@gmail.com>
2023-10-24 14:49:33 +0700

    merge from api-terminate-user

--- routes/users.js
+++ routes/users.js
@@ -5,12 +5,12 @@ const {
   terUser,
 } = require('../app/controllers/userController');
 var router = express.Router();
+const mid = require('../app/middlewares/restrict');
 
 /* GET users listing. */
 
 router.post('/', mid.auth, addUser);
 router.post('/login', login);
-
 router.patch('/:id', terUser);
 
 module.exports = router;
