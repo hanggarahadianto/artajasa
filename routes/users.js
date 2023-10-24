9a2d63ff415f2df868666863a9cf5d682c1dcd46
Dimasdidanfebianto <dimaskanjeng113@gmail.com>
2023-10-24 14:01:51 +0700

    patch status deactive

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,13 @@
 var express = require('express');
-const { addUser, login } = require('../app/controllers/userController');
+const { addUser, login, terUser } = require('../app/controllers/userController');
 var router = express.Router();
 
 /* GET users listing. */
 
 router.post('/', addUser);
 router.post('/login', login);
+
+
+router.patch('/:id', terUser);
 
 module.exports = router;
