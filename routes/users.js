afc571a0ab7b3060fcd198fed8f403c16be0f59c
Lintangeaa <lindprak@gmail.com>
2023-10-24 10:20:59 +0700

    create login api

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
-const { addUser } = require('../app/controllers/userController');
+const { addUser, login } = require('../app/controllers/userController');
 var router = express.Router();
 
 /* GET users listing. */
 
 router.post('/', addUser);
+router.post('/login', login);
 
 module.exports = router;
