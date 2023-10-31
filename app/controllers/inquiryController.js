3291a66ecb036e94cb723bb3c167310c0bfc49b5
Lintangeaa <lindprak@gmail.com>
2023-10-31 14:12:05 +0700

    push

--- app/controllers/inquiryController.js
+++ app/controllers/inquiryController.js
@@ -35,9 +35,15 @@ exports.addInquiry = catchAsync(async (req, res) => {
 
   try {
     const response = await axios.post(`${SVIP_URL}/inquiry`, data, config);
-    const responseData = response.data;
+    const responseData = response.data.QRInquiryQrisRS;
 
-    res.json({ status: true, response: responseData });
+    if (responseData.responseCode != '00') {
+      res
+        .status(400)
+        .json({ status: false, message: 'Error', response: response.data });
+    }
+
+    res.json({ status: true, response: response.data });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: error.message });
