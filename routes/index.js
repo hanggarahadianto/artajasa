33c7c8b600c5f0de563f2d010f442e02d17d9b16
Lintangeaa <lindprak@gmail.com>
2023-10-31 16:20:11 +0700

    push

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-
+router.get('/ping', (req, res) => {
+  res.status(200).json({
+    status: true,
+    message: 'Pong',
+  });
+});
 module.exports = router;
